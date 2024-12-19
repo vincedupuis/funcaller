28dc5da708e59ed0f5e1e3f290486f8c3c2f2bdb
VincentD <vincent.dupuis@zimmerbiomet.com>
2024-12-18 21:07:03 -0500

    add comments. while loop to empty queue.

--- src/ThreadedFunctionQueue.cpp
+++ src/ThreadedFunctionQueue.cpp
@@ -4,22 +4,29 @@ using namespace funcall;
 
 std::thread::id ThreadedFunctionQueue::start() noexcept
 {
+    // Prevent multiple threads from starting/stopping the queue at the same time
     std::scoped_lock lock(mMutex);
 
+    // If the context is already created, it means the thread is already running
+    // Return the thread ID (already started)
     auto ctx = mContext.load();
     if (ctx)
         return ctx->thread.get_id();
 
+    // Create a new context and start the thread
     ctx = std::make_shared<Context>();
     ctx->thread = std::thread(&ThreadedFunctionQueue::processQueue, ctx, mLogger);
 
+    // Wait for the thread to start.
+    // If it starts, store the context and return the thread ID
     waitFor(ctx->running, true);
     if (ctx->running) {
         mContext.store(ctx);
         return ctx->thread.get_id();
     }
 
-    ctx->stopping = true;
+    // If the thread did not start, signal stopping and try to detach it
+    ctx->stopping.store(true);
     ctx->condition.notify_one();
     try {
         ctx->thread.detach();
@@ -28,27 +35,36 @@ std::thread::id ThreadedFunctionQueue::start() noexcept
         if (mLogger)
             mLogger(e.what());
     }
+
+    // Return an empty thread ID
     return {};
 }
 
 bool ThreadedFunctionQueue::stop() noexcept
 {
+    // Prevent multiple threads from starting/stopping the queue at the same time
     std::scoped_lock lock(mMutex);
 
+    // If the context is not created, this means the thread is not running
+    // Return true (already stopped)
     const auto ctx = mContext.load();
     if (!ctx)
         return true;
 
-    ctx->stopping = true;
+    // Signal stopping and wait for the thread to stop
+    ctx->stopping.store(true);
     ctx->condition.notify_one();
     waitFor(ctx->running, false);
 
+    // If the thread stopped, join it and clear the context
+    // Return true (stopped)
     if (!ctx->running) {
         ctx->thread.join();
         mContext.store(nullptr);
         return true;
     }
 
+    // The thread did not stop, try to detach it
     try {
         ctx->thread.detach();
     }
@@ -57,25 +73,33 @@ bool ThreadedFunctionQueue::stop() noexcept
             mLogger(e.what());
     }
 
+    // Clear the context and return false (not stopped)
     mContext.store(nullptr);
     return false;
 }
 
 void ThreadedFunctionQueue::add(Function &&function) noexcept
 {
+    // Note: if the stop occurred during this call, the last
+    // context will be deleted at the end of this function
+
+    // If the context is not created, this means the thread is not running.
     const auto ctx = mContext.load();
     if (!ctx)
         return;
 
+    // Add the function to the queue and notify the thread
     std::unique_lock lock(ctx->mutex);
     ctx->queue.push(std::move(function));
+    lock.unlock();
     ctx->condition.notify_one();
 }
 
 void ThreadedFunctionQueue::waitFor(const std::atomic_bool &var, const bool value) noexcept
 {
+    // Wait for the variable to reach the desired value before timeout
     const auto start = std::chrono::system_clock::now();
-    while (var.load() != value) {
+    while (var != value) {
         if (std::chrono::system_clock::now() - start > std::chrono::seconds(5))
             break;
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -86,34 +110,41 @@ void ThreadedFunctionQueue::processQueue(
     std::shared_ptr<Context> context,
     std::function<void(std::string &&)> _logger) noexcept
 {
-    const auto logger = std::move(_logger);
+    // Move the context and logger to the local variables
     const auto ctx = std::move(context);
+    const auto logger = std::move(_logger);
 
+    // Acquire the lock and notify the thread is running
     std::unique_lock lock(ctx->mutex);
-    ctx->running = true;
+    ctx->running.store(true);
 
+    // Process the queue until stopping is signaled
     while (!ctx->stopping) {
+        // Wait for the queue to have elements or stopping to be signaled
         ctx->condition.wait(lock, [&] {
             return ctx->stopping || !ctx->queue.empty();
         });
 
-        if (ctx->stopping) {
-            break;
-        }
+        // Empty the queue and execute the functions while the thread is not stopping
+        while (!ctx->stopping && !ctx->queue.empty())
+        {
+            // Get the function from the queue
+            auto function = std::move(ctx->queue.front());
+            ctx->queue.pop();
 
-        auto function = std::move(ctx->queue.front());
-        ctx->queue.pop();
-
-        ctx->mutex.unlock();
-        try {
-            function();
+            // Execute the function while not holding the lock
+            ctx->mutex.unlock();
+            try {
+                function();
+            }
+            catch (const std::exception &e) {
+                if (logger)
+                    logger(e.what());
+            }
+            ctx->mutex.lock();
         }
-        catch (const std::exception &e) {
-            if (logger)
-                logger(e.what());
-        }
-        ctx->mutex.lock();
     }
 
-    ctx->running = false;
+    // Notify the thread is not running
+    ctx->running.store(false);
 }
--- src/ThreadedFunctionQueue.h
+++ src/ThreadedFunctionQueue.h
@@ -4,12 +4,14 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <functional>
 #include <mutex>
-#include <queue>
 #include <thread>
+#include <queue>
 
 namespace funcall {
 
+// A thread-safe object that queues and execute functions in a separate thread.
 class ThreadedFunctionQueue final : public IFunctionQueue
 {
 public:
@@ -17,17 +19,27 @@ public:
         : mLogger(nullptr)
     {}
 
+    // Constructor with a static logger function
     explicit ThreadedFunctionQueue(void (*log)(std::string &&)) noexcept
         : mLogger(log)
     {}
 
+    // Destructor, will stop the thread if running
     ~ThreadedFunctionQueue() noexcept override { stop(); }
 
+    // Start the thread and return the thread id,
+    // or return an empty id if the thread is already running
+    // or if the thread could not be started
     std::thread::id start() noexcept;
+
+    // Stop the thread and return true if the thread is stopped,
+    // or return false if the thread is not running or could not be stopped
     bool stop() noexcept;
 
+    // Add a function to the queue. It will be executed in the thread
     void add(Function &&function) noexcept override;
 
+    // Copy and move constructors and assignment operators are not allowed
     ThreadedFunctionQueue(const ThreadedFunctionQueue &) = delete;
     ThreadedFunctionQueue &operator=(const ThreadedFunctionQueue &) = delete;
     ThreadedFunctionQueue(ThreadedFunctionQueue &&) = delete;
@@ -36,6 +48,7 @@ public:
 private:
     std::function<void(std::string &&)> mLogger;
 
+    // Define the context needed for the thread
     struct Context
     {
         std::thread thread;
@@ -46,14 +59,18 @@ private:
         std::atomic_bool stopping = false;
     };
 
+    // Mutex to protect the start and stop functions
     std::mutex mMutex;
+
+    // Atomic shared pointer to the current thread context
     std::atomic<std::shared_ptr<Context>> mContext;
 
+    // Wait for the variable to reach the desired value before timeout
     static void waitFor(const std::atomic_bool &var, bool value) noexcept;
 
+    // Entry point for the thread to process the queue
     static void processQueue(
-        std::shared_ptr<Context> context,
-        std::function<void(std::string &&)> _logger) noexcept;
+        std::shared_ptr<Context> context, std::function<void(std::string &&)> _logger) noexcept;
 };
 
 } // namespace funcall
